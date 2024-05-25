49feddfc1a4fd9e5b0b76d1694d1d5249048f2aa
bugrahanyapilmisev <bugrahan.yapilmisev@sabanciuniv.edu>
2024-05-26 02:48:23 +0300

    Update average_ready_in_time.py

--- For web_app and maps/average_ready_in_time.py
+++ For web_app and maps/average_ready_in_time.py
@@ -9,7 +9,7 @@ def create_ready_in_time_map():
     fig = px.choropleth(
         df,
         locations="ISO-3",
-        color="Average Ready In Per Recipe",
+        color="Avg Ready In",
         hover_name="Country",
         color_continuous_scale=px.colors.sequential.Plasma,
         range_color=(55.762019230769226, 298.47842744291285),
